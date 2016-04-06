30c1292a932b6b61fc0e2aa2d1f4c75bc1c8095f
Karl-Axel Zander <kazander@kth.se>
2016-04-06 10:17:19 +0200

    v1.0 upplagd av spelet

--- karl-axels_cmd-adventure/Program.cs
+++ karl-axels_cmd-adventure/Program.cs
@@ -10,6 +10,40 @@ namespace karl_axels_cmd_adventure
     {
         static void Main(string[] args)
         {
+            Boolean harNyckel = false;
+            string input;
+
+            Console.WriteLine( "---Välkommen till Karl-Axels Command Line Adventure---");
+            Console.WriteLine("\nDu står i ett tomt rum. Detta enda som finns är en dörr framför dig. Det ligger en nyckel på golvet");
+
+            while (true) //oändlig loop
+            {
+                Console.Write("\n>");
+                input = Console.ReadLine();
+
+                if (input == "dörr" || input == "öppna dörr"  || input == "gå till dörr")
+                {
+                    if (harNyckel)
+                    {
+                        Console.WriteLine("Du öppnar dörren och går igenom");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Den är låst");
+                }
+
+                else if (input == "nyckel" || input == "ta nyckel")
+                {
+                    Console.WriteLine("Du tog nyckeln");
+                    harNyckel = true;
+                }
+                else
+                    Console.WriteLine("Icke gilltigt eller felstavat commando!");
+
+            }
+
+            Console.WriteLine("\nDu vann!!! Spelet stängs av...");
+            Console.ReadLine();
         }
     }
 }
